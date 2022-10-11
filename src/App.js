e2b35f4f6eb7241696e6f88fd8c75a96c583edde
kim kyunghun <tirhande@gmail.com>
2022-10-11 16:18:54 +0900

    style: css 수정

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/login';
 import RegistPage from './pages/register/register'
-import { createGlobalStyle } from "styled-components";
 import NotFound from './pages/notfound';
 import TodoPage from './pages/todo/todo';
 import PrivateRoute from './lib/privateRoute';
+import { GlobalStyles } from './styles/styles';
 
-const GlobalStyles = createGlobalStyle`
-  * {
-    margin:0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Nanum Gothic';
-  }
-  html, body, div#root {
-    height: 100%;
-  }
-  body {
-    font-size: 1rem;
-    line-height: 1.6;
-    background: #fafafa;
-  }
-  button {
-    cursor: pointer;
-  }
-  button:disabled {
-    cursor: default;
-  }
-  @font-face {
-    font-family: 'Nanum Gothic';
-    font-style: normal;
-    font-weight: 400;
-    src: url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.eot);
-    src: url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.eot?#iefix) format('embedded-opentype'),
-         url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.woff2) format('woff2'),
-         url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.woff) format('woff'),
-         url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.ttf) format('truetype');
-  }
-`;
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter basename="wanted-pre-onboarding-fe-7">
         <Routes>
           <Route path="/" element={<PrivateRoute component={<LoginPage />} />}/>
           <Route path="/register" element={<RegistPage />} />
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,4 +1,37 @@
-import styled, { css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Nanum Gothic';
+  }
+  html, body, div#root {
+    height: 100%;
+  }
+  body {
+    font-size: 1rem;
+    line-height: 1.6;
+    background: #fafafa;
+  }
+  button {
+    cursor: pointer;
+  }
+  button:disabled {
+    cursor: default;
+  }
+  @font-face {
+    font-family: 'Nanum Gothic';
+    font-style: normal;
+    font-weight: 400;
+    src: url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.eot);
+    src: url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.eot?#iefix) format('embedded-opentype'),
+         url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.woff2) format('woff2'),
+         url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.woff) format('woff'),
+         url(//themes.googleusercontent.com/static/fonts/earlyaccess/nanumgothic/v3/NanumGothic-Regular.ttf) format('truetype');
+  }
+`;
 
 const CommonStyle = css`
   height: 100%;
